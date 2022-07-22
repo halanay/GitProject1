3e4b88df97c5d0ebe63ad5d363f08c00c86f9d53
halanay <hamdialanay@gmail.com>
2022-07-22 21:59:09 +0300

    args committing test

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -3,8 +3,9 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World Work1 is done");
         System.out.println("Created Git Repository");
-
         System.out.println("Devoloping hello feature");
+
+
 
     }
 }
