e8ffb19c308c8c55ccac5eced26687ac2e8adc23
halanay <hamdialanay@gmail.com>
2022-07-22 13:04:55 +0300

    added more work

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -2,5 +2,6 @@ public class Hello {
 
     public static void main(String[] args) {
         System.out.println("Hello World Work1 is done");
+        System.out.println("Created Git Repository");
     }
 }
